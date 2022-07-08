95846bd9e36f93b60eaf1c175f2d3331276730f8
Ctnn <yigitcetinkaya2002@hotmail.com>
2022-07-08 23:10:08 +0300

    More State Build

--- src/screens/ChangebleScreen.js
+++ src/screens/ChangebleScreen.js
@@ -1,30 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 import {Text,Button,View,StyleSheet} from "react-native";
 
 function ButtonSection(props)
 {
-    return <Button title={props.verilecekad}/>
+    return <View>
+<Button title={`More ${props.verilecekad}`} onPress={()=>props.onMore}/>
+<Button title={`Less ${props.verilecekad}`} onPress={()=>props.onLess}/>
+    </View>;
 }
 
 function ChangebleScreen()
 {
+    const[red,setRed]=useState(0);
+    const[green,setGreen]=useState(0);
+    const[blue,setBlue]=useState(0);
+
 return (<View>
 <View>
     <Text style={styles.text}>Red Section</Text>
-    <ButtonSection verilecekad="More Red"/>
-    <ButtonSection verilecekad="Less Red"/>
+    <ButtonSection onMore={()=>setRed(red+1)} onLess={()=>setRed(red-1)} verilecekad="Red"/>
 
 </View>
 <View>
     <Text style={styles.text}>Green Section</Text>
-    <ButtonSection verilecekad="More Green"/>
-    <ButtonSection verilecekad="Less Green"/>
+    <ButtonSection onMore={()=>setGreen(green+1)}  onLess={()=>setGreen(green-1)} verilecekad="Green"/>
 
 </View>
 <View>
     <Text style={styles.text}>Blue Section</Text>
-    <ButtonSection verilecekad="More Blue"/>
-    <ButtonSection verilecekad="Less Blue"/>
+    <ButtonSection onMore={()=>setBlue(blue+1)} onLess={()=>setBlue(blue-1)} verilecekad="Blue"/>
 
 </View>
 </View>);
